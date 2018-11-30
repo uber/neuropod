bb55c55f1b71b197926ca0e304fdea73a3c3fdb7
Vivek Panyam <vip@uber.com>
2018-11-30 05:44:53 +0000

    NeuropodOutputData for Neuropods C++ Interface [redacted]
    [redacted]
    
    Reviewed By: yevgeni

---
+++ source/neuropods/neuropod_output_data.cc
@@ -0,0 +1,58 @@
+//
+// Uber, Inc. (c) 2018
+//
+
+#include "neuropod_output_data.hh"
+
+#include "neuropods/internal/neuropod_tensor.hh"
+#include "neuropods/internal/tensor_store.hh"
+
+namespace neuropods
+{
+
+NeuropodOutputData::NeuropodOutputData(std::unique_ptr<TensorStore> tensor_store)
+    : tensor_store(std::move(tensor_store))
+{
+}
+
+NeuropodOutputData::~NeuropodOutputData() = default;
+
+template <typename T>
+void NeuropodOutputData::get_data_pointer_and_size(const std::string &node_name, const T *&pointer, size_t &size) const
+{
+    auto tensor = tensor_store->find(node_name);
+
+    pointer = boost::get<T *>(tensor->get_data_ptr());
+    size    = tensor->get_num_elements();
+}
+
+template <typename T>
+std::vector<T> NeuropodOutputData::get_data_as_vector(const std::string &node_name) const
+{
+    std::vector<T> out;
+
+    // Get the size and a pointer to the data
+    size_t   size;
+    const T *data_pointer;
+    get_data_pointer_and_size(node_name, data_pointer, size);
+
+    // Copy into the vector
+    out.insert(out.end(), &data_pointer[0], &data_pointer[size]);
+
+    return out;
+}
+
+std::vector<int64_t> NeuropodOutputData::get_shape(const std::string &node_name) const
+{
+    return tensor_store->find(node_name)->get_dims();
+}
+
+// Instantiate the templates
+#define INIT_TEMPLATES_FOR_TYPE(CPP_TYPE, NEUROPOD_TYPE)                             \
+    template void NeuropodOutputData::get_data_pointer_and_size(                     \
+        const std::string &node_name, const CPP_TYPE *&pointer, size_t &size) const; \
+    template std::vector<CPP_TYPE> NeuropodOutputData::get_data_as_vector(const std::string &node_name) const;
+
+FOR_EACH_TYPE_MAPPING(INIT_TEMPLATES_FOR_TYPE);
+
+} // namespace neuropods
---
+++ source/neuropods/neuropod_output_data.hh
@@ -0,0 +1,41 @@
+//
+// Uber, Inc. (c) 2018
+//
+
+#pragma once
+
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace neuropods
+{
+
+// This is opaque to the user
+struct TensorStore;
+
+// The return type of `infer`
+class NeuropodOutputData
+{
+protected:
+    // Hidden internal storage
+    std::unique_ptr<TensorStore> tensor_store;
+
+public:
+    explicit NeuropodOutputData(std::unique_ptr<TensorStore> tensor_store);
+    ~NeuropodOutputData();
+
+    // Get a pointer to the underlying data
+    template <typename T>
+    void get_data_pointer_and_size(const std::string &node_name, const T *&pointer, size_t &size) const;
+
+    // Get the data as a vector
+    // Note: this operation requires a copy
+    template <typename T>
+    std::vector<T> get_data_as_vector(const std::string &node_name) const;
+
+    // Get the shape of a tensor
+    std::vector<int64_t> get_shape(const std::string &node_name) const;
+};
+
+} // namespace neuropods
