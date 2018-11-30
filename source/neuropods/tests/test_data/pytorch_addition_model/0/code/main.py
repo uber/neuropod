d522fac41d7e694fddfd9b403b89abcf5945f134
Vivek Panyam <vip@uber.com>
2018-11-30 11:04:29 +0000

    Tests for Neuropods C++ to Python Bridge [redacted]
    [redacted]
    
    Reviewed By: yevgeni

---
+++ source/neuropods/tests/test_data/pytorch_addition_model/0/code/main.py
@@ -0,0 +1,12 @@
+import torch.nn as nn
+
+
+class AdditionModel(nn.Module):
+    def forward(self, x, y):
+        return {
+            "out": x + y
+        }
+
+
+def get_model(_):
+    return AdditionModel()
