e4cc4fc9483b0056dc8d56198b1956baedbe6661
Vivek Panyam <vip@uber.com>
2019-01-11 02:10:44 +0000

    Make NumpyNeuropodTensor header-only [redacted]
    [redacted]
    
    Reviewed By: yevgeni

--- source/neuropods/backends/python_bridge/numpy_neuropod_tensor.hh
+++ source/neuropods/backends/python_bridge/numpy_neuropod_tensor.hh
@@ -18,8 +18,54 @@ namespace neuropods
 
 namespace py = boost::python;
 
+namespace
+{
+
+void setup_numpy_if_needed()
+{
+    // Needed in order for numpy arrays to work from c++
+    if (PyArray_API == nullptr)
+    {
+        import_array();
+        py::numeric::array::set_module_and_type("numpy", "ndarray");
+    }
+}
+
+std::vector<int64_t> get_dims_from_numpy(PyArrayObject *nparray)
+{
+    // Get info about the numpy array
+    int       ndims = PyArray_NDIM(nparray);
+    npy_intp *dims  = PyArray_DIMS(nparray);
+
+    // Create a vector with the shape info
+    std::vector<int64_t> shape(&dims[0], &dims[ndims]);
+
+    return shape;
+}
+
+} // namespace
+
 // Utility function to get an nparray from a boost object
-PyArrayObject *get_nparray_from_obj(py::object boost_obj);
+PyArrayObject *get_nparray_from_obj(py::object boost_obj)
+{
+    setup_numpy_if_needed();
+
+    PyObject *obj = boost_obj.ptr();
+    if (obj == nullptr || !PyArray_Check(obj))
+    {
+        throw std::runtime_error("Can't extract data from python object; expected numpy array");
+    }
+
+    PyArrayObject *arr = reinterpret_cast<PyArrayObject *>(obj);
+
+    // Make sure it's contiguous and aligned
+    // It is the caller's responsibility to decref the returned array
+    Py_INCREF(PyArray_DESCR(arr));
+    PyArrayObject *carr = reinterpret_cast<PyArrayObject *>(
+        PyArray_FromAny(obj, PyArray_DESCR(arr), 0, 0, NPY_ARRAY_CARRAY_RO, nullptr));
+
+    return carr;
+}
 
 // This class is internal to neuropods and should not be exposed
 // to users
@@ -28,20 +74,55 @@ class NumpyNeuropodTensor : public TypedNeuropodTensor<T>, public NativeDataCont
 {
 public:
     // Allocate a numpy array
-    NumpyNeuropodTensor(const std::string &name, const std::vector<int64_t> &dims);
+    NumpyNeuropodTensor(const std::string &name, const std::vector<int64_t> &dims) : TypedNeuropodTensor<T>(name, dims)
+    {
+        setup_numpy_if_needed();
+
+        // Allocate the numpy array
+        PyObject *obj = PyArray_SimpleNew(
+            // num dims
+            dims.size(),
+
+            // The dimensions
+            const_cast<npy_intp *>(dims.data()),
+
+            // numpy typenum
+            get_numpy_type_from_neuropod_type(get_tensor_type_from_cpp<T>()));
+
+        py::handle<>       handle(obj);
+        py::numeric::array arr(handle);
+
+        nparray_ = arr;
+    };
 
     // Wrap an existing array
-    NumpyNeuropodTensor(const std::string &name, PyArrayObject *nparray);
+    NumpyNeuropodTensor(const std::string &name, PyArrayObject *nparray)
+        : TypedNeuropodTensor<T>(name, get_dims_from_numpy(nparray))
+    {
+        py::handle<>       handle(reinterpret_cast<PyObject *>(nparray));
+        py::numeric::array arr(handle);
 
-    ~NumpyNeuropodTensor();
+        nparray_ = arr;
+    }
+
+
+    ~NumpyNeuropodTensor() = default;
 
     // Get a pointer to the underlying data
-    T *get_raw_data_ptr();
+    T *get_raw_data_ptr()
+    {
+        auto arr = reinterpret_cast<PyArrayObject *>(nparray_.ptr());
 
-    py::object get_native_data();
+        // Get a pointer to the underlying data
+        void *data = PyArray_DATA(arr);
+
+        return static_cast<T *>(data);
+    }
+
+    py::object get_native_data() { return nparray_; }
 
     // The underlying numpy array
-    py::object nparray;
+    py::object nparray_;
 };
 
 } // namespace neuropods
