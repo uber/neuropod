086cb353e509e1a3adf17f33e6bdc3a2abc4776c
Vivek Panyam <vip@uber.com>
2019-01-11 02:06:11 +0000

    Make TorchNeuropodTensor header-only [redacted]
    [redacted]
    
    Reviewed By: yevgeni

--- source/neuropods/backends/torchscript/torch_tensor.hh
+++ source/neuropods/backends/torchscript/torch_tensor.hh
@@ -15,6 +15,41 @@
 namespace neuropods
 {
 
+namespace
+{
+
+template <typename T>
+T *get_data_from_torch_tensor(const torch::Tensor &tensor)
+{
+    return tensor.data<T>();
+}
+
+template <>
+std::string *get_data_from_torch_tensor(const torch::Tensor &tensor)
+{
+    throw std::runtime_error("String support is not implemented yet");
+}
+
+template <>
+uint16_t *get_data_from_torch_tensor(const torch::Tensor &tensor)
+{
+    throw std::runtime_error("TorchScript doesn't support type uint16_t");
+}
+
+template <>
+uint32_t *get_data_from_torch_tensor(const torch::Tensor &tensor)
+{
+    throw std::runtime_error("TorchScript doesn't support type uint32_t");
+}
+
+template <>
+uint64_t *get_data_from_torch_tensor(const torch::Tensor &tensor)
+{
+    throw std::runtime_error("TorchScript doesn't support type uint64_t");
+}
+
+} // namespace
+
 // This class is internal to neuropods and should not be exposed
 // to users
 template <typename T>
@@ -23,17 +58,24 @@ class TorchNeuropodTensor : public TypedNeuropodTensor<T>, public NativeDataCont
 public:
     // Allocate a torch tensor
     // TODO(vip): maybe add a way to wrap existing data using torch::from_blob
-    TorchNeuropodTensor(const std::string &name, const std::vector<int64_t> &dims);
+    TorchNeuropodTensor(const std::string &name, const std::vector<int64_t> &dims)
+        : TypedNeuropodTensor<T>(name, dims),
+          tensor(torch::empty(dims, get_torch_type_from_neuropod_type(get_tensor_type_from_cpp<T>())))
+    {
+    }
 
     // Wrap an existing torch tensor
-    TorchNeuropodTensor(const std::string &name, torch::Tensor tensor);
+    TorchNeuropodTensor(const std::string &name, torch::Tensor tensor)
+        : TypedNeuropodTensor<T>(name, tensor.sizes().vec()), tensor(tensor)
+    {
+    }
 
-    ~TorchNeuropodTensor();
+    ~TorchNeuropodTensor() = default;
 
     // Get a pointer to the underlying data
-    T *get_raw_data_ptr();
+    T *get_raw_data_ptr() { return get_data_from_torch_tensor<T>(tensor); }
 
-    torch::jit::IValue get_native_data();
+    torch::jit::IValue get_native_data() { return tensor; }
 
     // The underlying torch tensor
     torch::Tensor tensor;
