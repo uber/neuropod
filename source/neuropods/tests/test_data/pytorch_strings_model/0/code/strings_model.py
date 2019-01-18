d28abca2692d2e96c20677eb204625568d98ff96
Vivek Panyam <vip@uber.com>
2019-01-18 04:50:35 +0000

    Add test models for string tensors (C++) [redacted]
    [redacted]
    
    Reviewed By: yevgeni

---
+++ source/neuropods/tests/test_data/pytorch_strings_model/0/code/strings_model.py
@@ -0,0 +1,15 @@
+
+import numpy as np
+import torch
+import torch.nn as nn
+
+
+class StringsModel(nn.Module):
+    def forward(self, x, y):
+        return {
+            "out": np.array([f + " " + s for f, s in zip(x, y)])
+        }
+
+
+def get_model(_):
+    return StringsModel()
