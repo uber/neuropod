f4071f3313bee671e6b7cbb3a69e4fb6aa7eceb8
Yevgeni Litvin <yevgeni@uber.com>
2019-02-05 21:37:05 -0800

    Do not rely on static variable initialization order in the backend registrar.

--- source/neuropods/internal/backend_registration.cc
+++ source/neuropods/internal/backend_registration.cc
@@ -5,6 +5,7 @@
 #include "backend_registration.hh"
 
 #include <iostream>
+#include <mutex>
 #include <sstream>
 #include <stdexcept>
 #include <unordered_map>
@@ -15,8 +16,17 @@ namespace neuropods
 namespace
 {
 
-std::unordered_map<std::string, BackendFactoryFunction> registered_backends_by_type;
-std::unordered_map<std::string, BackendFactoryFunction> registered_backends_by_name;
+std::once_flag registrar_initialized;
+std::unordered_map<std::string, BackendFactoryFunction>* registered_backends_by_type = nullptr;
+std::unordered_map<std::string, BackendFactoryFunction>* registered_backends_by_name = nullptr;
+
+void init_registrar_if_needed()
+{
+    std::call_once(registrar_initialized, [](){
+        registered_backends_by_name = new std::unordered_map<std::string, BackendFactoryFunction>();
+        registered_backends_by_type = new std::unordered_map<std::string, BackendFactoryFunction>();
+    });    
+}
 
 } // namespace
 
@@ -24,20 +34,24 @@ bool register_backend(const std::string &             name,
                       const std::vector<std::string> &supported_types,
                       BackendFactoryFunction          factory_fn)
 {
+    init_registrar_if_needed();
+
     for (const std::string &type : supported_types)
     {
-        registered_backends_by_type[type] = factory_fn;
+        (*registered_backends_by_type)[type] = factory_fn;
     }
 
-    registered_backends_by_name[name] = factory_fn;
+    (*registered_backends_by_name)[name] = factory_fn;
 
     return true;
 }
 
 BackendFactoryFunction get_backend_for_type(const std::string &type)
 {
-    auto backend_it = registered_backends_by_type.find(type);
-    if (backend_it == registered_backends_by_type.end())
+    init_registrar_if_needed();
+
+    auto backend_it = registered_backends_by_type->find(type);
+    if (backend_it == registered_backends_by_type->end())
     {
         std::stringstream ss;
         ss << "Neuropod backend not found for type '" << type << "'! ";
@@ -52,8 +66,10 @@ BackendFactoryFunction get_backend_for_type(const std::string &type)
 
 BackendFactoryFunction get_backend_by_name(const std::string &name)
 {
-    auto backend_it = registered_backends_by_name.find(name);
-    if (backend_it == registered_backends_by_name.end())
+    init_registrar_if_needed();
+
+    auto backend_it = registered_backends_by_name->find(name);
+    if (backend_it == registered_backends_by_name->end())
     {
         std::stringstream ss;
         ss << "Neuropod backend not found for name'" << name << "'! ";
