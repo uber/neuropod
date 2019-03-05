fd6aeb16e4ad659e22337b7a36e8dd701840421d
Vivek Panyam <vip@uber.com>
2019-03-05 12:57:48 -0800

    Add string tensor support to the native TensorFlow backend (C++)

--- source/neuropods/tests/test_default_backend.cc
+++ source/neuropods/tests/test_default_backend.cc
@@ -34,3 +34,9 @@ TEST(test_default_backend, test_tensorflow_addition_model)
     // Test the TensorFlow addition model
     test_addition_model("neuropods/tests/test_data/tf_addition_model/");
 }
+
+TEST(test_default_backend, test_tensorflow_strings_model)
+{
+    // Test the TensorFlow strings model
+    test_strings_model("neuropods/tests/test_data/tf_strings_model/");
+}
