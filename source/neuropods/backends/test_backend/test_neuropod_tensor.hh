a31c177616a93c0acf52495539cd6c26cf2f090e
Vivek Panyam <vip@uber.com>
2019-03-15 18:41:53 -0700

    Add string tensor support to the test backend

--- source/neuropods/backends/test_backend/test_neuropod_tensor.hh
+++ source/neuropods/backends/test_backend/test_neuropod_tensor.hh
@@ -37,4 +37,30 @@ public:
     const T *get_raw_data_ptr() const { return static_cast<T *>(data_); }
 };
 
+// Specialization for strings
+template <>
+class TestNeuropodTensor<std::string> : public TypedNeuropodTensor<std::string>
+{
+private:
+    // The data contained in the tensor
+    std::vector<std::string> data_;
+
+public:
+    TestNeuropodTensor(const std::string &name, const std::vector<int64_t> &dims) : TypedNeuropodTensor<std::string>(name, dims)
+    {
+    }
+
+    ~TestNeuropodTensor() = default;
+
+    void set(const std::vector<std::string> &data)
+    {
+        data_ = data;
+    }
+
+    std::vector<std::string> get_data_as_vector()
+    {
+        return data_;
+    }
+};
+
 } // namespace neuropods
