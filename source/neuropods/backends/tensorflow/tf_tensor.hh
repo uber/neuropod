0c73804f626ac6ad2bc436cc10b7fce654c912de
Vivek Panyam <viv.panyam@gmail.com>
2019-03-18 22:22:27 -0500

    Add string tensor support to the native TensorFlow backend (C++) (#22)

--- source/neuropods/backends/tensorflow/tf_tensor.hh
+++ source/neuropods/backends/tensorflow/tf_tensor.hh
@@ -78,4 +78,141 @@ public:
     TF_Tensor *tensor;
 };
 
+
+// Specialization for strings
+template <>
+class TensorflowNeuropodTensor<std::string> : public TypedNeuropodTensor<std::string>,
+                                              public NativeDataContainer<TF_Tensor *>
+{
+public:
+    // Allocate a TF tensor
+    TensorflowNeuropodTensor(const std::string &name, const std::vector<int64_t> &dims)
+        : TypedNeuropodTensor<std::string>(name, dims)
+    {
+    }
+
+    // Wrap an existing TF tensor
+    TensorflowNeuropodTensor(const std::string &name, TF_Tensor *tensor)
+        : TypedNeuropodTensor<std::string>(name, get_shape(tensor)), tensor(tensor)
+    {
+    }
+
+    ~TensorflowNeuropodTensor()
+    {
+        if (tensor != nullptr)
+        {
+            TF_DeleteTensor(tensor);
+        }
+    }
+
+    void set(const std::vector<std::string> &data)
+    {
+        // The format of TF string tensors is described here:
+        // https://github.com/tensorflow/tensorflow/blob/master/tensorflow/c/c_api.h#L214
+
+        // Make sure that the size of the provided data matches the number of
+        // elements in the tensor
+        if (data.size() != this->get_num_elements())
+        {
+            throw std::runtime_error("Size of provided vector does not match the number "
+                "of elements in the tensor");
+        }
+
+        // Get the size of the index
+        const size_t index_size = sizeof(uint64_t) * data.size();
+
+        // Compute the total size of the tensor
+        size_t total_bytes = index_size;
+        for (const auto &item : data)
+        {
+            total_bytes += TF_StringEncodedSize(item.length());
+        }
+
+        // Allocate the tensor
+        const auto dims = get_dims();
+        tensor          = TF_AllocateTensor(TF_STRING, dims.data(), dims.size(), total_bytes);
+
+        // Get pointers to the data
+        void *    tensor_data_ptr = TF_TensorData(tensor);
+        uint64_t *index_ptr       = static_cast<uint64_t *>(tensor_data_ptr);
+        char *    data_ptr        = static_cast<char *>(tensor_data_ptr) + index_size;
+
+        // Set the data
+        TF_Status *status   = TF_NewStatus();
+        size_t     counter  = 0;
+        uint64_t   position = 0;
+        for (const auto &item : data)
+        {
+            // Get the lengths and set the index entry
+            const auto len         = item.length();
+            const auto encoded_len = TF_StringEncodedSize(len);
+            index_ptr[counter++]   = position;
+
+            // Encode the string
+            TF_StringEncode(item.c_str(), len, data_ptr + position, encoded_len, status);
+
+            // Check status
+            if (TF_GetCode(status) != TF_OK)
+            {
+                std::stringstream ss;
+                ss << "Tensorflow error: " << TF_Message(status);
+                throw std::runtime_error(ss.str());
+            }
+
+            // Move the pointer forward
+            position += encoded_len;
+        }
+
+        // Delete the status
+        TF_DeleteStatus(status);
+    }
+
+    std::vector<std::string> get_data_as_vector()
+    {
+        // The format of TF string tensors is described here:
+        // https://github.com/tensorflow/tensorflow/blob/master/tensorflow/c/c_api.h#L214
+        std::vector<std::string> out;
+
+        // Get pointers to the data and sizes
+        const size_t     tensor_size     = TF_TensorByteSize(tensor);
+        const size_t     numel           = get_num_elements();
+        void * const     tensor_data_ptr = TF_TensorData(tensor);
+        uint64_t * const index_ptr       = static_cast<uint64_t *>(tensor_data_ptr);
+        const size_t     index_size      = sizeof(uint64_t) * numel;
+        char * const     data_ptr        = static_cast<char *>(tensor_data_ptr) + index_size;
+
+        TF_Status *status = TF_NewStatus();
+        for (size_t i = 0; i < numel; i++)
+        {
+            const uint64_t position = index_ptr[i];
+            const char *   dst;
+            size_t         buf_len;
+
+            // Decode the string
+            TF_StringDecode(data_ptr + position, tensor_size - position - index_size, &dst, &buf_len, status);
+
+            // Check status
+            if (TF_GetCode(status) != TF_OK)
+            {
+                std::stringstream ss;
+                ss << "Tensorflow error: " << TF_Message(status);
+                throw std::runtime_error(ss.str());
+            }
+
+            // Add the string
+            out.emplace_back(dst, buf_len);
+        }
+
+        // Delete the status
+        TF_DeleteStatus(status);
+
+        return out;
+    }
+
+    TF_Tensor *get_native_data() { return tensor; }
+
+    // The underlying TF tensor
+    TF_Tensor *tensor;
+};
+
 } // namespace neuropods
--- source/neuropods/tests/test_tensorflow_backend.cc
+++ source/neuropods/tests/test_tensorflow_backend.cc
@@ -9,3 +9,9 @@ TEST(test_models, test_tensorflow_addition_model)
     // Test the TensorFlow addition model using the native TensorFlow backend
     test_addition_model("neuropods/tests/test_data/tf_addition_model/", "TensorflowNeuropodBackend");
 }
+
+TEST(test_models, test_tensorflow_strings_model)
+{
+    // Test the TensorFlow strings model using the native TensorFlow backend
+    test_strings_model("neuropods/tests/test_data/tf_strings_model/", "TensorflowNeuropodBackend");
+}
