4d20effe5413a9886fa4599babb600deaea9b96a
Vivek Panyam <vip@uber.com>
2019-06-13 11:52:39 -0700

    Add scaffolding for GPU tests in CI

--- build/install_python_deps.sh
+++ build/install_python_deps.sh
@@ -6,5 +6,12 @@ set -e
 pushd source/python
 pip install -U pip setuptools numpy
 python setup.py egg_info
-cat neuropods.egg-info/requires.txt | sed '/^\[/ d' | paste -sd " " - | xargs pip install -f https://download.pytorch.org/whl/nightly/cpu/torch_nightly.html
+
+if [[ $(uname -s) == 'Darwin' ]]; then
+    # Only CPU torch packages are provided on Mac
+    cat neuropods.egg-info/requires.txt | sed '/^\[/ d' | paste -sd " " - | xargs pip install -f https://download.pytorch.org/whl/nightly/cpu/torch_nightly.html
+else
+    # Install torch compatible with CUDA 10.0
+    cat neuropods.egg-info/requires.txt | sed '/^\[/ d' | paste -sd " " - | xargs pip install -f https://download.pytorch.org/whl/nightly/cu100/torch_nightly.html
+fi
 popd
