e41315b72809906067d3d2fed7d3c37fff2856a5
Alex Sergeev <asergeev@uber.com>
2019-07-26 22:13:10 -0700

    Don't skip TensorFlow 1.14.0 custom ops tests on Linux
    Signed-off-by: Alex Sergeev <asergeev@uber.com>

--- source/neuropods/python/tests/custom_ops/tensorflow/test_tensorflow_custom_ops.py
+++ source/neuropods/python/tests/custom_ops/tensorflow/test_tensorflow_custom_ops.py
@@ -5,6 +5,7 @@
 import numpy as np
 import os
 import subprocess
+import sys
 import tensorflow as tf
 import unittest
 from testpath.tempdir import TemporaryDirectory
@@ -29,7 +30,7 @@ def create_tf_addition_model(custom_op_path):
 
     return g.as_graph_def()
 
-@unittest.skipIf(tf.__version__ == '1.14.0',
+@unittest.skipIf(tf.__version__ == '1.14.0' and sys.platform == 'darwin',
                  'See https://github.com/tensorflow/tensorflow/issues/30633')
 class TestTensorflowCustomOps(unittest.TestCase):
     @classmethod
