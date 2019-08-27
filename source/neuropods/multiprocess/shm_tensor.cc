6e3402361a581b0cc4fd6b662e94fb33ffa2b259
Vivek Panyam <vip@uber.com>
2019-08-26 20:29:04 -0700

    [OPE Part 2.1] Shared memory tensor implementation

---
+++ source/neuropods/multiprocess/shm_tensor.cc
@@ -0,0 +1,13 @@
+//
+// Uber, Inc. (c) 2019
+//
+
+#include "neuropods/multiprocess/shm_tensor.hh"
+
+namespace neuropods
+{
+
+// The SHMAllocator used by all SHMNeuropodTensors
+SHMAllocator shm_allocator;
+
+} // namespace neuropods
