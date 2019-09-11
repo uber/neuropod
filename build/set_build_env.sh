e9ff30d769aaf706a043760d807e29a266904a71
Vivek Panyam <vip@uber.com>
2019-09-10 21:39:31 -0700

    Prepare for packaging of backends (#153)

--- build/set_build_env.sh
+++ build/set_build_env.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -e
 
-# Add TF and torch to the LD_LIBRARY_PATH
-# This is so the tests can find the shared objects at runtime
-# TODO(vip): find a better way to do this
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`pwd`/bazel-source/external/libtorch_repo/lib
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`pwd`/bazel-source/external/tensorflow_repo/lib
-
 # The backends need to be on the library path for the tests to dynamically load them
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`pwd`/bazel-bin/neuropods/backends/python_bridge/
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`pwd`/bazel-bin/neuropods/backends/torchscript/
