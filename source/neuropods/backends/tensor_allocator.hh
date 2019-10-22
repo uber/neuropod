bae93c972aaf77f259824b48ec20467387de6c2e
Vivek Panyam <vip@uber.com>
2019-10-22 13:09:44 -0700

    Add factory functions for creating tensors (#178)

--- source/neuropods/backends/tensor_allocator.hh
+++ source/neuropods/backends/tensor_allocator.hh
@@ -8,7 +8,9 @@
 #include "neuropods/internal/neuropod_tensor.hh"
 #include "neuropods/internal/tensor_types.hh"
 
+#include <algorithm>
 #include <memory>
+#include <random>
 #include <string>
 #include <vector>
 
@@ -54,6 +56,104 @@ public:
             this->tensor_from_memory(input_dims, get_tensor_type_from_cpp<T>(), data, deleter);
 
         return std::dynamic_pointer_cast<TypedNeuropodTensor<T>>(tensor);
+    }
+
+    // Returns a tensor of type `T` and shape `input_dims` filled with `fill_value`
+    template <typename T>
+    std::shared_ptr<TypedNeuropodTensor<T>> full(const std::vector<int64_t> &input_dims, T fill_value)
+    {
+        auto tensor = allocate_tensor<T>(input_dims);
+        std::fill_n(tensor->get_raw_data_ptr(), tensor->get_num_elements(), fill_value);
+        return tensor;
+    }
+
+    // Returns a tensor of type `T` and shape `input_dims` filled with zeros
+    template <typename T>
+    std::shared_ptr<TypedNeuropodTensor<T>> zeros(const std::vector<int64_t> &input_dims)
+    {
+        return full<T>(input_dims, 0);
+    }
+
+    // Returns a tensor of type `T` and shape `input_dims` filled with ones
+    template <typename T>
+    std::shared_ptr<TypedNeuropodTensor<T>> ones(const std::vector<int64_t> &input_dims)
+    {
+        return full<T>(input_dims, 1);
+    }
+
+    // Returns a 1D tensor of type `T` containing a sequence of numbers starting at `start`
+    // with a step size of `step`. The tensor has the following shape: (`ceil((end - start) / step)`)
+    template <typename T>
+    std::shared_ptr<TypedNeuropodTensor<T>> arange(T start, T end, T step = 1)
+    {
+        // Note: This is intended to be used in tests and therefore is not optimized for performance
+
+        // Compute the size and allocate a tensor
+        int64_t size = std::ceil((end - start) / static_cast<double>(step));
+        auto tensor = allocate_tensor<T>({size});
+
+        // Fill the tensor
+        auto data_ptr    = tensor->get_raw_data_ptr();
+        const auto numel = tensor->get_num_elements();
+        for (int i = 0, val = 0; i < numel; i++, val += step)
+        {
+            data_ptr[i] = start + val;
+        }
+
+        return tensor;
+    }
+
+    // Returns a 1D tensor of type `T` containing a sequence of numbers starting at `0` and
+    // ending at `end - 1` with a step size of `1`. The tensor has the following shape: (`ceil(end)`)
+    template <typename T>
+    std::shared_ptr<TypedNeuropodTensor<T>> arange(T end)
+    {
+        return arange<T>(0, end);
+    }
+
+    // Returns an identity matrix of type `T` and shape (`M`, `N`). This matrix has ones on the diagonal and
+    // zeros everywhere else.
+    template <typename T>
+    std::shared_ptr<TypedNeuropodTensor<T>> eye(int64_t M, int64_t N)
+    {
+        // Note: This is intended to be used in tests and therefore is not optimized for performance
+
+        auto smallest_dim = std::min(M, N);
+        auto tensor = zeros<T>({M, N});
+        auto accessor = tensor->template accessor<2>();
+
+        for (int i = 0; i < smallest_dim; i++)
+        {
+            accessor[i][i] = 1;
+        }
+
+        return tensor;
+    }
+
+    // Returns a tensor of type `T` and shape `input_dims` filled with random numbers from a normal
+    // distribution with mean `mean` and standard deviation `stddev`.
+    template <typename T>
+    std::shared_ptr<TypedNeuropodTensor<T>> randn(const std::vector<int64_t> &input_dims, T mean = 0, T stddev = 1)
+    {
+        // Note: This is intended to be used in tests and therefore is not optimized for performance
+
+        // Allocate a tensor
+        auto tensor = allocate_tensor<T>(input_dims);
+
+        // Setup random number generation
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::normal_distribution<T> d(mean, stddev);
+
+        // Fill the tensor with random numbers
+        auto data_ptr    = tensor->get_raw_data_ptr();
+        const auto numel = tensor->get_num_elements();
+        for (int i = 0; i < numel; i++)
+        {
+            data_ptr[i] = d(gen);
+        }
+
+        return tensor;
     }
 };
 
---
+++ source/neuropods/tests/test_factories.cc
@@ -0,0 +1,84 @@
+//
+// Uber, Inc. (c) 2019
+//
+
+#include "gtest/gtest.h"
+#include "neuropods/backends/test_backend/test_neuropod_backend.hh"
+
+namespace
+{
+
+template <typename T>
+bool is_full(neuropods::TypedNeuropodTensor<T> &tensor, T value, size_t num_items)
+{
+    // Make sure our size is correct
+    auto actual_numel = tensor.get_num_elements();
+    EXPECT_EQ(actual_numel, num_items);
+
+    // Generate expected data
+    T expected_data[num_items];
+    std::fill_n(expected_data, num_items, value);
+
+    // Compare
+    auto actual_data = tensor.get_raw_data_ptr();
+    return memcmp(actual_data, expected_data, num_items * sizeof(uint8_t)) == 0;
+}
+
+template <typename T>
+bool matches_expected(neuropods::TypedNeuropodTensor<T> &tensor, const std::vector<T> expected)
+{
+    return tensor.get_data_as_vector() == expected;
+}
+
+} // namespace
+
+TEST(test_factories, test_factories)
+{
+    neuropods::TestNeuropodBackend backend;
+
+    auto allocator = backend.get_tensor_allocator();
+
+    constexpr size_t           num_items = 60;
+    const std::vector<int64_t> dims      = {3, 4, 5};
+
+    auto zeros = allocator->zeros<uint16_t>(dims);
+    auto ones = allocator->ones<int32_t>(dims);
+    auto full = allocator->full<double>(dims, 1.23);
+    auto randn = allocator->randn<float>(dims);
+
+    EXPECT_TRUE(is_full<uint16_t>(*zeros, 0, num_items));
+    EXPECT_TRUE(is_full<int32_t>(*ones, 1, num_items));
+    EXPECT_TRUE(is_full<double>(*full, 1.23, num_items));
+
+    // It's super unlikely for every element to be 0
+    EXPECT_FALSE(is_full<float>(*randn, 0, num_items));
+
+    auto range1 = allocator->arange<float>(5);
+    auto range2 = allocator->arange<float>(2, 6);
+    auto range3 = allocator->arange<float>(0, 10, 2);
+
+    // Make sure they're 1D tensors
+    EXPECT_EQ(range1->get_dims().size(), 1);
+    EXPECT_EQ(range2->get_dims().size(), 1);
+    EXPECT_EQ(range3->get_dims().size(), 1);
+
+    // And that they match the expected data
+    EXPECT_TRUE(matches_expected(*range1, {0, 1, 2, 3, 4}));
+    EXPECT_TRUE(matches_expected(*range2, {2, 3, 4, 5}));
+    EXPECT_TRUE(matches_expected(*range3, {0, 2, 4, 6, 8}));
+
+    auto eye1 = allocator->eye<float>(4, 4);
+    EXPECT_TRUE(matches_expected(*eye1, {
+        1, 0, 0, 0,
+        0, 1, 0, 0,
+        0, 0, 1, 0,
+        0, 0, 0, 1
+    }));
+
+    auto eye2 = allocator->eye<float>(3, 7);
+    EXPECT_TRUE(matches_expected(*eye2, {
+        1, 0, 0, 0, 0, 0, 0,
+        0, 1, 0, 0, 0, 0, 0,
+        0, 0, 1, 0, 0, 0, 0
+    }));
+}
