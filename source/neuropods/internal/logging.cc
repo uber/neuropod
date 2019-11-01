33493b0e1d41764756e5afbcb538d9d2d31c179e
Vivek Panyam <vip@uber.com>
2019-11-01 04:07:01 -0700

    Switch logging to spdlog

--- source/neuropods/internal/logging.cc
+++ source/neuropods/internal/logging.cc
@@ -4,11 +4,7 @@
 
 #include "neuropods/internal/logging.hh"
 
-#include <atomic>
-#include <chrono>
-#include <cstdlib>
 #include <iostream>
-#include <sstream>
 
 namespace neuropods
 {
@@ -16,75 +12,50 @@ namespace neuropods
 namespace
 {
 // Get a default log level from the environment
-LogSeverity get_default_log_level()
+spdlog::level::level_enum get_default_log_level()
 {
     const char *log_level_cstr = std::getenv("NEUROPOD_LOG_LEVEL");
     if (log_level_cstr == nullptr)
     {
-        return LogSeverity::INFO;
+        return spdlog::level::info;
     }
 
     std::string log_level(log_level_cstr);
     if (log_level == "DEBUG")
     {
-        return LogSeverity::DEBUG;
+        return spdlog::level::debug;
     }
 
     if (log_level == "INFO")
     {
-        return LogSeverity::INFO;
+        return spdlog::level::info;
     }
 
     if (log_level == "WARN")
     {
-        return LogSeverity::WARN;
+        return spdlog::level::warn;
     }
 
     if (log_level == "ERROR")
     {
-        return LogSeverity::ERROR;
+        return spdlog::level::err;
     }
 
     std::cerr << "Warning: Invalid value for NEUROPOD_LOG_LEVEL: " << log_level << ". Falling back to INFO"
               << std::endl;
-    return LogSeverity::INFO;
+    return spdlog::level::info;
 }
 
-std::atomic<LogSeverity> current_log_level{get_default_log_level()};
+bool set_initial_log_level()
+{
+    spdlog::set_level(get_default_log_level());
+    spdlog::set_pattern("%D %T.%f: %L %@] [thread %t, process %P] %v");
+
+    return true;
+}
+
+bool current_log_level = set_initial_log_level();
 
 } // namespace
-
-void set_log_level(LogSeverity severity)
-{
-    current_log_level = severity;
-}
-
-void log(LogSeverity severity, const char *fname, int line, const std::string &message)
-{
-    if (severity < current_log_level)
-    {
-        // Don't log
-        return;
-    }
-
-    // Based on https://github.com/tensorflow/tensorflow/blob/master/tensorflow/core/platform/default/logging.cc#L96
-    const auto now_micros = std::chrono::duration_cast<std::chrono::microseconds>(
-                                std::chrono::high_resolution_clock::now().time_since_epoch())
-                                .count();
-    const time_t  now_seconds      = static_cast<time_t>(now_micros / 1000000);
-    const int32_t micros_remainder = static_cast<int32_t>(now_micros % 1000000);
-    const size_t  time_buffer_size = 30;
-    char          time_buffer[time_buffer_size];
-    strftime(time_buffer, time_buffer_size, "%Y-%m-%d %H:%M:%S", localtime(&now_seconds));
-
-    fprintf(stderr,
-            "%s.%06d: %c %s:%d] %s\n",
-            time_buffer,
-            micros_remainder,
-            "DIWE"[static_cast<int>(severity)],
-            fname,
-            line,
-            message.c_str());
-}
 
 } // namespace neuropods
--- source/neuropods/multiprocess/ipc_control_channel.cc
+++ source/neuropods/multiprocess/ipc_control_channel.cc
@@ -87,7 +87,7 @@ void IPCControlChannel::send_message(control_message &msg)
 {
     // Make sure that it is valid to go from the previous message type to the current one
     verifier_.assert_transition_allowed(msg.type);
-    NEUROPOD_LOG_DEBUG("OPE: Sending message " << msg.type);
+    SPDLOG_DEBUG("OPE: Sending message {}", msg.type);
     send_queue_->send(&msg, sizeof(control_message), 0);
 }
 
@@ -156,7 +156,7 @@ void IPCControlChannel::recv_message(control_message &received)
     unsigned int priority;
     recv_queue_->receive(&received, sizeof(control_message), received_size, priority);
 
-    NEUROPOD_LOG_DEBUG("OPE: Received message " << received.type);
+    SPDLOG_DEBUG("OPE: Received message {}", received.type);
 
     // Make sure that it is valid to go from the previous message type to the current one
     verifier_.assert_transition_allowed(received.type);
@@ -176,7 +176,7 @@ bool IPCControlChannel::recv_message(control_message &received, size_t timeout_m
         recv_queue_->timed_receive(&received, sizeof(control_message), received_size, priority, timeout_at);
     if (successful_read)
     {
-        NEUROPOD_LOG_DEBUG("OPE: Received message " << received.type);
+        SPDLOG_DEBUG("OPE: Received message {}", received.type);
 
         // Make sure that it is valid to go from the previous message type to the current one
         verifier_.assert_transition_allowed(received.type);
