389cc92287a41a11782bd5f9f89dafeb6e96ea2b
Vivek Panyam <vip@uber.com>
2019-11-06 15:02:11 -0800

    Remove apply_visitor from NeuropodTensor (#201)

---
+++ source/neuropods/internal/neuropod_tensor_raw_data_access.cc
@@ -0,0 +1,27 @@
+//
+// Uber, Inc. (c) 2019
+//
+
+#include "neuropods/internal/neuropod_tensor_raw_data_access.hh"
+
+#include "neuropods/internal/neuropod_tensor.hh"
+
+namespace neuropods
+{
+
+void * NeuropodTensorRawDataAccess::get_untyped_data_ptr(NeuropodTensor &tensor)
+{
+    return tensor.get_untyped_data_ptr();
+}
+
+const void * NeuropodTensorRawDataAccess::get_untyped_data_ptr(const NeuropodTensor &tensor)
+{
+    return tensor.get_untyped_data_ptr();
+}
+
+size_t NeuropodTensorRawDataAccess::get_bytes_per_element(const NeuropodTensor &tensor)
+{
+    return tensor.get_bytes_per_element();
+}
+
+} // namespace neuropods
--- source/neuropods/internal/neuropod_tensor_serialization.cc
+++ source/neuropods/internal/neuropod_tensor_serialization.cc
@@ -4,6 +4,7 @@
 
 #include "neuropods/backends/tensor_allocator.hh"
 #include "neuropods/internal/neuropod_tensor.hh"
+#include "neuropods/internal/neuropod_tensor_raw_data_access.hh"
 #include "neuropods/serialization/serialization.hh"
 
 #include <boost/serialization/array.hpp>
@@ -15,45 +16,24 @@ namespace neuropods
 namespace
 {
 
-struct serialize_visitor : public NeuropodTensorVisitor<void>
-{
-    template <typename T, class Archive>
-    void operator()(const TypedNeuropodTensor<T> *tensor, Archive &ar) const
-    {
-        ar &boost::serialization::make_array(tensor->get_raw_data_ptr(), tensor->get_num_elements());
-    }
-
-    template <class Archive>
-    void operator()(const TypedNeuropodTensor<std::string> *tensor, Archive &ar) const
-    {
-        const auto data = tensor->get_data_as_vector();
-        ar &       data;
-    }
-};
-
-struct deserialize_visitor : public NeuropodTensorVisitor<void>
-{
-    template <typename T, class Archive>
-    void operator()(TypedNeuropodTensor<T> *tensor, Archive &ar) const
-    {
-        ar &boost::serialization::make_array(tensor->get_raw_data_ptr(), tensor->get_num_elements());
-    }
-
-    template <class Archive>
-    void operator()(TypedNeuropodTensor<std::string> *tensor, Archive &ar) const
-    {
-        std::vector<std::string> data;
-        ar &                     data;
-        tensor->set(data);
-    }
-};
-
 void serialize_tensor(const NeuropodTensor &tensor, boost::archive::binary_oarchive &ar)
 {
     int tensor_type = static_cast<int>(tensor.get_tensor_type());
     ar << tensor_type;
     ar << tensor.get_dims();
-    tensor.apply_visitor(serialize_visitor{}, ar);
+
+    if (tensor.get_tensor_type() == STRING_TENSOR)
+    {
+        const auto data = tensor.as_typed_tensor<std::string>()->get_data_as_vector();
+        ar << data;
+    }
+    else
+    {
+        ar << boost::serialization::make_array(
+            static_cast<const uint8_t *>(NeuropodTensorRawDataAccess::get_untyped_data_ptr(tensor)),
+            tensor.get_num_elements() * NeuropodTensorRawDataAccess::get_bytes_per_element(tensor)
+        );
+    }
 }
 
 std::shared_ptr<NeuropodTensor> deserialize_tensor(boost::archive::binary_iarchive &ar,
@@ -65,7 +45,20 @@ std::shared_ptr<NeuropodTensor> deserialize_tensor(boost::archive::binary_iarchi
     ar >> dims;
 
     auto out = allocator.allocate_tensor(dims, static_cast<TensorType>(type));
-    out->apply_visitor(deserialize_visitor{}, ar);
+    if (out->get_tensor_type() == STRING_TENSOR)
+    {
+        std::vector<std::string> data;
+        ar >> data;
+        out->as_typed_tensor<std::string>()->set(data);
+    }
+    else
+    {
+        ar >> boost::serialization::make_array(
+            static_cast<uint8_t *>(NeuropodTensorRawDataAccess::get_untyped_data_ptr(*out)),
+            out->get_num_elements() * NeuropodTensorRawDataAccess::get_bytes_per_element(*out)
+        );
+    }
+
     return out;
 }
 
