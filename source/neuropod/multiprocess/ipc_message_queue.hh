d0f78793cf353ddd1f6774c6694d8decaa9b821b
Vivek Panyam <vip@uber.com>
2020-03-04 22:50:17 -0800

    Switch queue implementation

--- source/neuropod/multiprocess/ipc_message_queue.hh
+++ source/neuropod/multiprocess/ipc_message_queue.hh
@@ -10,11 +10,11 @@
 
 #include <boost/any.hpp>
 #include <boost/interprocess/ipc/message_queue.hpp>
-#include <rigtorp/MPMCQueue.h>
 
 #include <atomic>
 #include <chrono>
 #include <condition_variable>
+#include <queue>
 #include <thread>
 #include <unordered_map>
 
@@ -53,6 +53,81 @@ enum QueueMessageType
 extern std::atomic_uint64_t msg_counter;
 
 } // namespace detail
+
+// A bounded blocking spsc queue
+template <typename T>
+class BlockingSPSCQueue
+{
+private:
+    std::queue<T>           queue_;
+    std::condition_variable full_cv_;
+    std::condition_variable empty_cv_;
+    std::mutex              mutex_;
+
+    size_t capacity_;
+
+public:
+    BlockingSPSCQueue(size_t capacity) : capacity_(capacity) {}
+    ~BlockingSPSCQueue() = default;
+
+    bool try_emplace(T &&item)
+    {
+        bool success = false;
+
+        // Lock the mutex and add to the queue if we have capacity
+        {
+            std::unique_lock<std::mutex> lock(mutex_);
+            if (queue_.size() < capacity_)
+            {
+                queue_.emplace(item);
+                success = true;
+            }
+        }
+
+        // Notify any waiting read threads if we need to
+        if (success)
+        {
+            empty_cv_.notify_all();
+        }
+
+        return success;
+    }
+
+    void emplace(T &&item)
+    {
+        // Lock the mutex and add to the queue (or wait until we have capacity)
+        {
+            std::unique_lock<std::mutex> lock(mutex_);
+            if (queue_.size() >= capacity_)
+            {
+                full_cv_.wait(lock, [&] { return queue_.size() < capacity_; });
+            }
+
+            queue_.emplace(item);
+        }
+
+        // Notify any waiting read threads
+        empty_cv_.notify_all();
+    }
+
+    void pop(T &item)
+    {
+        // Lock the mutex and get an item from the queue (or wait until we have an item)
+        {
+            std::unique_lock<std::mutex> lock(mutex_);
+            if (queue_.empty())
+            {
+                empty_cv_.wait(lock, [&] { return !queue_.empty(); });
+            }
+
+            item = std::move(queue_.front());
+            queue_.pop();
+        }
+
+        // Notify any waiting write threads
+        full_cv_.notify_all();
+    }
+};
 
 // Forward declare IPCMessageQueue
 template <typename>
@@ -212,7 +287,7 @@ private:
     using Message = QueueMessage<UserPayloadType>;
 
     // Output messages
-    rigtorp::MPMCQueue<Message> out_queue_;
+    BlockingSPSCQueue<Message>     out_queue_;
 
     // Internal IPC queues
     std::string                         control_queue_name_;
