d0f72d7d586c84caf8bd94cb0aefacf8770d0571
Vivek Panyam <vip@uber.com>
2020-03-05 02:09:35 -0800

    Fix serialization

--- source/neuropod/multiprocess/ipc_serialization.cc
+++ source/neuropod/multiprocess/ipc_serialization.cc
@@ -13,39 +13,63 @@ namespace neuropod
 template <>
 void ipc_serialize(std::ostream &out, const NeuropodValueMap &data)
 {
+    // Write the number of items
+    size_t num_items = data.size();
+    out.write(reinterpret_cast<const char *>(&num_items), sizeof(num_items));
+
     std::unordered_map<std::string, SHMBlockID> id_map;
     for (const auto &entry : data)
     {
+        // Write the length of the name
+        size_t name_len = entry.first.size();
+        out.write(reinterpret_cast<const char *>(&name_len), sizeof(name_len));
+
+        // Write the name
+        out.write(entry.first.c_str(), name_len);
+
+        // Write the block ID
         const auto &block_id =
             std::dynamic_pointer_cast<NativeDataContainer<SHMBlockID>>(entry.second)->get_native_data();
 
-        id_map[entry.first] = block_id;
+        out.write(reinterpret_cast<const char *>(block_id.data()), 24);
     }
-
-    boost::archive::binary_oarchive ar{out};
-    ar << id_map;
 }
 
 template <>
 void ipc_deserialize(std::istream &in, NeuropodValueMap &data)
 {
-    std::unordered_map<std::string, SHMBlockID> id_map;
-    boost::archive::binary_iarchive             ar{in};
-    ar >> id_map;
+    // Read the number of items
+    size_t num_items;
+    in.read(reinterpret_cast<char *>(&num_items), sizeof(num_items));
 
-    for (const auto &entry : id_map)
+    // TODO: check string length
+    char tmp[256];
+
+    for (int i = 0; i < num_items; i++)
     {
+        // Read the length of the name
+        size_t name_len;
+        in.read(reinterpret_cast<char *>(&name_len), sizeof(name_len));
+
+        // Read the name
+        in.read(tmp, name_len);
+        std::string name(tmp, name_len);
+
+        // Read the block ID
+        SHMBlockID block_id;
+        in.read(reinterpret_cast<char *>(block_id.data()), 24);
+
         // Make sure we're not overwriting anything
-        auto &item = data[entry.first];
+        auto &item = data[name];
         if (!item)
         {
             // Create a tensor
-            item = tensor_from_id(entry.second);
+            item = tensor_from_id(block_id);
         }
         else
         {
             NEUROPOD_ERROR("When receiving a message as part of OPE, attempted to overwrite a tensor with name {}",
-                           entry.first);
+                           name);
         }
     }
 }
