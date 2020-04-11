9c3be22ad177b421dcc3222bc1f2e7e57f33b565
Vivek Panyam <vip@uber.com>
2020-04-10 21:47:23 -0400

    Bump version to 0.2.0rc1 (#335)
    Update the Neuropod version to `0.2.0rc1`

--- source/neuropod/version.hh
+++ source/neuropod/version.hh
@@ -21,13 +21,13 @@ namespace neuropod
 
 // These are allowed to be 4 bits each
 #define NEUROPOD_RELEASE_LEVEL NEUROPOD_RELEASE_LEVEL_RELEASE_CANDIDATE
-#define NEUROPOD_RELEASE_SERIAL 0
+#define NEUROPOD_RELEASE_SERIAL 1
 
 static_assert(NEUROPOD_RELEASE_LEVEL < 16, "NEUROPOD_RELEASE_LEVEL must be in the range 0 to 15 (4 bits)");
 static_assert(NEUROPOD_RELEASE_SERIAL < 16, "NEUROPOD_RELEASE_SERIAL must be in the range 0 to 15 (4 bits)");
 
 // The version as a string
-#define NEUROPOD_VERSION "0.2.0rc0"
+#define NEUROPOD_VERSION "0.2.0rc1"
 
 // Version as a single 4-byte hex number, e.g. 0x010502B2 == 1.5.2b2.
 // Use this for numeric comparisons
