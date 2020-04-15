a379900a7c7ed07404d021ffabc2fdb055fa0d90
Vivek Panyam <vip@uber.com>
2020-04-15 16:33:47 -0400

    [OPE] Add benchmark for round-robin with multiple instances

--- source/neuropod/tests/benchmark_multiprocess.cc
+++ source/neuropod/tests/benchmark_multiprocess.cc
@@ -87,3 +87,41 @@ void benchmark_small_inputs(benchmark::State &state)
 
 BENCHMARK_TEMPLATE(benchmark_small_inputs, load_in_process);
 BENCHMARK_TEMPLATE(benchmark_small_inputs, load_out_of_process);
+
+void benchmark_ope_scaling_round_robin(benchmark::State &state)
+{
+    const uint8_t some_image_data[1200 * 1920 * 3] = {0};
+
+    // Create models
+    std::vector<neuropod::Neuropod> models;
+    for (int i = 0; i < state.range(0); i++)
+    {
+        neuropod::RuntimeOptions opts;
+        opts.use_ope = true;
+        models.emplace_back("neuropod/tests/test_data/dummy_object_detection/", opts);
+    }
+
+    size_t counter    = 0;
+    size_t num_models = models.size();
+    for (auto _ : state)
+    {
+        // Simple round robin
+        auto &neuropod = models.at(counter);
+        counter        = (counter + 1) % num_models;
+
+        neuropod::NeuropodValueMap input_data;
+
+        // Add an input "image"
+        auto image_tensor = neuropod.allocate_tensor<uint8_t>({1200, 1920, 3});
+        image_tensor->copy_from(some_image_data, 1200 * 1920 * 3);
+        input_data["image"] = image_tensor;
+
+        // Run inference
+        const auto output_data = neuropod.infer(input_data);
+
+        // Make sure we don't optimize it out
+        benchmark::DoNotOptimize(output_data);
+    }
+}
+
+BENCHMARK(benchmark_ope_scaling_round_robin)->DenseRange(1, 8);
