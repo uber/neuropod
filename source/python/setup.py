db27e06f3df4a4796821a513e2f02d5ac142af72
Vivek Panyam <vip@uber.com>
2020-05-29 16:25:05 -0400

    Bump version to 0.2.0rc2

--- source/python/setup.py
+++ source/python/setup.py
@@ -10,7 +10,7 @@ class BinaryDistribution(Distribution):
 
 setup(
     name="neuropod",
-    version="0.2.0rc1",
+    version="0.2.0rc2",
     install_requires=REQUIRED_PACKAGES,
     packages=find_packages(),
     package_data={'': ["neuropod_native.so", "libneuropod.so", "neuropod_multiprocess_worker"]},
