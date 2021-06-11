5ab305aeb4f22d520a8095a6696c11696ea23379
Vivek Panyam <viv.panyam@gmail.com>
2021-06-11 03:22:33 -0400

    Allow filtering native tests based on framework

--- source/neuropod/backends/torchscript/test/test_torchscript_backend.cc
+++ source/neuropod/backends/torchscript/test/test_torchscript_backend.cc
@@ -53,3 +53,15 @@ TEST(test_torchscript_backend, invalid_dtype)
 //     // Load a TorchScript model directly
 //     neuropod::TorchNeuropodBackend model("neuropod/tests/test_data/torchscript_strings_model/0/data/model.pt");
 // }
+
+TEST(test_multiprocess_backend, test_torchscript_addition_model)
+{
+    // Test the TorchScript addition model in another process
+    test_addition_model_ope("neuropod/tests/test_data/torchscript_addition_model/");
+}
+
+TEST(test_multiprocess_backend, test_torchscript_strings_model)
+{
+    // Test the TorchScript strings model in another process
+    test_strings_model_ope("neuropod/tests/test_data/torchscript_strings_model/");
+}
