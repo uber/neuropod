3ffabbd41cd369422703e2072ee91b55dd8c6e0c
Vivek Panyam <viv.panyam@gmail.com>
2022-03-07 20:55:58 -0500

    Modify copyright headers and add an AUTHORS file

--- source/python/neuropod/tests/utils.py
+++ source/python/neuropod/tests/utils.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 
-# Copyright (c) 2020 UATC, LLC
+# Copyright (c) 2020 The Neuropod Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
